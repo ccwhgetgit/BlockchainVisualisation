0d4bf3e13270a9d7b76f67a67ca3f73a11af692e
ccwhgetgit <65432561+ccwhgetgit@users.noreply.github.com>
2021-11-24 22:24:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import time
 import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import date
+from io import BytesIO
 
 page = st.selectbox("Choose your page", ["IMX", "L1 Network Activities", "OpenSea Rarity"])
 if page == "IMX":
@@ -111,8 +112,9 @@ if page == "IMX":
     st.write("Number of NFT Projects on IMX = " + number)
 
 if page == "L1 Network Activities":
-    st.title("Ethereum, Avalanche, Fantom, Elrond Network")
+    st.title("Avalanche, Algorand, Fantom, Elrond Network")
     st.write("Avalanche and Fantom has a 2 day delay in their explorer")
+    st.write("Algorand's explorer has to be updated 4 months later")
     url = "https://snowtrace.io/chart/tx"
 
     # Make a GET request to fetch the raw HTML content
@@ -122,12 +124,12 @@ if page == "L1 Network Activities":
     soup = BeautifulSoup(html_content, "html.parser")
     a = soup.prettify()
 
-    start = a.find('Monday, November 1, 2021')
+    start = a.find('October 1, 2021')
     end = a.find('Highcharts')
     a = a[start:end]
     a = a.split()
     # avalanche
-    d = pd.DataFrame(columns=['blocktime', 'avatxnactivity', 'avanewaddress', 'ftmtxnactivity', 'ftmnewaddress', 'elrondtxnactivity', 'elrondnewaddress'])
+    d = pd.DataFrame(columns=['blocktime', 'avatxnactivity', 'avanewaddress', 'ftmtxnactivity', 'ftmnewaddress', 'elrondtxnactivity', 'elrondnewaddress', 'algorandtxnactivity', 'algorandnewaddress'])
     count = 0
     total = 0
 
@@ -158,7 +160,7 @@ if page == "L1 Network Activities":
             count += 1
     count = 0
 
-    d.loc[count, 'blocktime'] = '1 Nov 2020'
+
 
 
     url = "https://ftmscan.com/chart/tx"
@@ -169,7 +171,7 @@ if page == "L1 Network Activities":
     # Parse the html content
     soup = BeautifulSoup(html_content, "html.parser")
     a = soup.prettify()
-    start = a.find('Monday, November 1, 2021')
+    start = a.find('October 1, 2021')
     end = a.find('Highcharts')
     a = a[start:end]
     a = a.split()
@@ -202,7 +204,7 @@ if page == "L1 Network Activities":
             d.loc[count, 'ftmtxnactivity'] = int(sa)
             count += 1
 
-    d.loc[0, 'blocktime'] = '1 November 2020'
+    d.loc[0, 'blocktime'] = '1 June 2020'
 
 # fantom
     #elrond
@@ -215,7 +217,7 @@ if page == "L1 Network Activities":
     soup = BeautifulSoup(html_content, "html.parser")
     a = soup.prettify()
 
-    start = a.find('"time":"2021-11-01T00:00:00.000Z"')
+    start = a.find('"time":"2021-10-01T00:00:00.000Z"')
     b = a[start:].split("time")
     count = 0
     total = 0
@@ -236,12 +238,12 @@ if page == "L1 Network Activities":
     soup = BeautifulSoup(html_content, "html.parser")
     a = soup.prettify()
 
-    start = a.find('"time":"2021-11-01T00:00:00.000Z"')
+    start = a.find('"time":"2021-10-01T00:00:00.000Z"')
     b = a[start:].split("time")
 
     count = 0
     total = 0
-    initial = 856108
+    initial = 799298
     temp = 0
     for i in range(1, len(b)):
         l = b[i][35:]
@@ -253,6 +255,7 @@ if page == "L1 Network Activities":
                 sa += str(j)
 
         if count ==0:
+
             d.loc[count, 'elrondnewaddress'] = int(sa) - initial
             temp = int(sa)
         else:
@@ -269,16 +272,112 @@ if page == "L1 Network Activities":
         d.loc[count, 'blocktime'] = l
         count += 1
 
+    import datetime
+
+    url = "https://indexer.algoexplorerapi.io/stats/v2/transactions/count?time-start=1621983374&interval=1D"
+
+    # Make a GET request to fetch the raw HTML content
+    html_content = requests.get(url).text
+
+    # Parse the html content
+    soup = BeautifulSoup(html_content, "html.parser")
+    a = soup.prettify()
+
+    start = a.find('"time-start":1632960000')
+    a = a[start:].split("time")
+
+
+    count = 0
+    total = 0
+
+
+
+    count = 0
+    for i in range(2, len(a), 2):
+        c = list(a[i])
+        sa = ""
+        l = a[i][29:]
+        for i in range(len(l)):
+            if l[i].isdigit():
+                sa += l[i]
+            else:
+                break
+        d.loc[count, 'algorandtxnactivity'] = int(sa)
+        count += 1
+
+    url = "https://indexer.algoexplorerapi.io/stats/v2/accounts/balances?time-start=1621983259&interval=1D"
+
+    # Make a GET request to fetch the raw HTML content
+    html_content = requests.get(url).text
+
+    # Parse the html content
+    soup = BeautifulSoup(html_content, "html.parser")
+    a = soup.prettify()
+
+    start = a.find('"time-start":1632960000')
+    a = a[start:].split("time")
+
+    initial = 13605221
+    count = 0
+    before = 0
+    for i in range(2, len(a), 2):
+        c = list(a[i])
+        sa = ""
+        l = a[i][31:]
+        for j in range(len(l)):
+            if l[j].isdigit():
+                sa += l[j]
+            else:
+                break
+        if i == 2:
+
+            d.loc[count, 'algorandnewaddress'] = int(sa) - initial
+            before = int(sa)
+        else:
+
+            d.loc[count, 'algorandnewaddress'] = int(sa) - before
+            before = int(sa)
+        count += 1
+
     d
+
+
+    def to_excel(df):
+        output = BytesIO()
+        writer = pd.ExcelWriter(output, engine='xlsxwriter')
+        df.to_excel(writer, sheet_name='Sheet1')
+        writer.save()
+        processed_data = output.getvalue()
+        return processed_data
+
+
+    def get_table_download_link(df):
+        """Generates a link allowing the data in a given panda dataframe to be downloaded
+        in:  dataframe
+        out: href string
+        """
+        val = to_excel(df)
+        b64 = base64.b64encode(val)  # val looks like b'...'
+        return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="L1Networks.xlsx">Download csv file</a>'  # decode b'abc' => abc
+
+
+    df = d# your dataframe
+    st.markdown(get_table_download_link(df), unsafe_allow_html=True)
+
+
     plt.figure(figsize=(12,5))
-    df =d[['avanewaddress', 'ftmnewaddress', 'elrondnewaddress']].dropna()
-    st.write("Unique Daily Addresses for November")
+    d = d.dropna()
+    index = d['blocktime']
+
+    df =d[['avanewaddress', 'ftmnewaddress', 'elrondnewaddress', 'algorandnewaddress']].dropna()
+    df = df.set_index(index)
+    st.write("New Daily Addresses from 1 October 2021")
     st.line_chart(df)
 
-    df = d[['avatxnactivity', 'ftmtxnactivity', 'elrondtxnactivity']].dropna()
-    st.write("Unique Daily Transactions for November")
+    df = d[['avatxnactivity', 'ftmtxnactivity', 'elrondtxnactivity','algorandtxnactivity']].dropna()
+    df = df.set_index(index)
+    st.write("Daily Transaction Activity from 1 October 2021")
     st.line_chart(df)
-
 
 if page == "OpenSea Rarity":
 
