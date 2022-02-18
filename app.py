a84534d6c322c8cd5459d09cedcda5343514bc44
ccwhgetgit <65432561+ccwhgetgit@users.noreply.github.com>
2022-02-18 09:40:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ if page == "L1/L2 Network Activities":
 
     st.write("Networks supported: Ethereum, Terra, Harmony, Avalanche, Algorand, Fantom, Elrond, Polygon, Arbitrum, Solana")
     d = pd.DataFrame(
-        columns=['blocktime',  'ethtxnactivity', 'terratxnactivity', 'harmonytxnactivity', 'polytxnactivity', 'polynewaddress', 'arbitxnactivity',
+        columns=['blocktime',  'ethtxnactivity', 'terratxnactivity', 'solanatxnactivity', 'harmonytxnactivity', 'polytxnactivity', 'polynewaddress', 'arbitxnactivity',
                  'arbinewaddress', 'avatxnactivity', 'avanewaddress', 'ftmtxnactivity', 'ftmnewaddress',
                  'elrondtxnactivity',
                  'elrondnewaddress', 'algorandtxnactivity', 'algorandnewaddress'])
@@ -519,13 +519,13 @@ if page == "L1/L2 Network Activities":
     count = 0
     
 #For Harmony#
+
     url = "https://api.flipsidecrypto.com/api/v2/queries/5904f304-70dc-40af-9144-817dda7d1bf6/data/latest"
     html_content = requests.get(url).text
     soup = BeautifulSoup(html_content, "html.parser")
     a = soup.prettify()
     a = list(a.split('TX_ID'))
-    count = 0 
-
+    count = 92 - len(a)
     for i in range(len(a)): 
         l = a[i]
         value = ""
@@ -546,6 +546,36 @@ if page == "L1/L2 Network Activities":
             value1 = int(value1) 
             d.loc[count, 'harmonytxnactivity'] = value1
             count += 1
+            
+#For Solana#
+
+    url = "https://api.flipsidecrypto.com/api/v2/queries/ca8e74d7-7dd1-459a-aba4-1663f075c686/data/latest"
+    html_content = requests.get(url).text
+    soup = BeautifulSoup(html_content, "html.parser")
+    a = soup.prettify()
+    a = list(a.split('TX_ID'))
+    count = 92 - len(a)
+    for i in range(len(a)): 
+        l = a[i]
+        value = ""
+        value1 = ""
+        for j in range(len(l)): 
+            if l[j].isdigit(): 
+                value += l[j]
+            if l[j] =="D": 
+                break 
+        if num_there(value) == False: 
+            continue 
+        else: 
+            for k in range(len(value)): 
+                if value[k].isdigit(): 
+                    value1 += value[k]
+                else: 
+                    value1 = 0 
+            value1 = int(value1) 
+            d.loc[count, 'solanatxnactivity'] = value1
+            count += 1
+        
 
     #For Terra#
     #for terra
@@ -635,7 +665,7 @@ if page == "L1/L2 Network Activities":
     st.write("New Addresses")
     st.line_chart(df)
 
-    df = d[['ethtxnactivity', 'terratxnactivity', 'harmonytxnactivity', 'polytxnactivity', 'arbitxnactivity', 'avatxnactivity', 'ftmtxnactivity', 'elrondtxnactivity',
+    df = d[['ethtxnactivity', 'terratxnactivity', 'harmonytxnactivity', 'solanatxnactivity', 'polytxnactivity', 'arbitxnactivity', 'avatxnactivity', 'ftmtxnactivity', 'elrondtxnactivity',
             'algorandtxnactivity']].dropna()
     df = df.set_index(index)
     st.write("Txn Activity")
